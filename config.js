c454d0db620a0e1ad55782660cc6999620c1c503
selamdeep <selamdeep@gmail.com>
2025-09-17 21:53:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Basit fiyat ve iletişim konfigürasyonu
+window.sitePricing = {
+  hour: 25,   // ₺/saat
+  day: 150,   // ₺/gün
+  week: 900,  // ₺/hafta
+  month: 3200 // ₺/ay
+};
+
+// Aşağıdaki telefon numarasını ülke koduyla doldurun, ardından index.html içindeki tüm WhatsApp linkleri otomatik güncellenebilir bir sisteme geçirilebilir.
+// Bu basit sürümde linkler statik. Dilerseniz burada global bir değişken tanımlayıp dinamik set edebilirsiniz:
+// window.whatsapp = "90XXXXXXXXXX";
